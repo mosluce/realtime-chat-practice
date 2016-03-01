e59917710d140570a6a04edc5a2490e937b6c6f0
mosluce <mosluce@gmail.com>
2016-03-01 17:41:06 +0800

    prototyping

---
+++ socket.io.js
@@ -0,0 +1,18 @@
+module.exports = function (server) {
+    var io = require('socket.io')(server);
+    var mongo = require('socket.io-adapter-mongo');
+
+    io.adapter(mongo('mongodb://localhost/play-chat'));
+
+    io.on('connection', function (socket) {
+        socket.on('join', function(data) {
+            data.created_at = new Date();
+            socket.emit('join', data);
+        });
+
+        socket.on('message', function(data) {
+            data.created_at = new Date();
+            socket.emit('message', data);
+        });
+    });
+};
